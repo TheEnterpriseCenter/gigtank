b890f0e2ba4024b52e35ffe5d52bfd9346440bdd
shannahutch <shannahutch@gmail.com>
2016-07-21 15:33:28 -0400

    Adding styles to index.html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import render_template, request,flash, redirect, url_for
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-
+import models 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -10,14 +10,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # print(os.environ['APP_SETTINGS'])
 
-import models 
 
 @app.route('/')
 def index():
-    return "GigTank!"
+    return render_template('/index.html')
 
-@app.route('/portal')
+@app.route('/portal', methods=['POST', 'GET'])
 def portal():
+    if request.method == 'POST':
+        video=Video(request.form['title'], request.form['slide_summary'])
+        db.session.add(video)
+        db.session.commit()
+        flash('New entry was successfully posted')     
+
     return render_template('/portal.html')
 
 if __name__ == '__main__':
