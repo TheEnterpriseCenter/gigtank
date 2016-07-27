a6298380977807d63b6edf6b5c0b463c34b38fab
shanna <shanna@bbys-MacBook-Pro.local>
2016-07-27 15:01:15 -0400

    Fixing deploy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import render_template, request, flash, redirect, url_for, redirect
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-import models 
+# import models 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
