931ce919312a28215ada4aabdf75e7b3763670b7
shanna <shanna@bbys-MacBook-Pro.local>
2016-07-27 14:21:18 -0400

    Heroku config continued

--- videos.py
+++
@@ -1,6 +0,0 @@
-Videos = [
-    {'title': 'Nature', 'url': 'https://youtu.be/6pxRHBw-k8M'},
-    {'title': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'title': 'AOL', 'url': 'http://openid.aol.com/<usertitle>'},
-    {'title': 'Flickr', 'url': 'http://www.flickr.com/<usertitle>'},
-    {'title': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
